a84e4c5e068f92e836c6788fdb72059e2497d666
kyunghyunKim <hyunshiness@gmail.com>
2022-03-30 19:19:02 +0900

    ADD : 로그인 CSS 작업 완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+//import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 import '../../styles/common.scss';
 import '../../styles/reset.scss';
 
 const Login = () => {
-  const navigate = useNavigate();
-  const onLogin = () => {
-    navigate('/Main/Main.');
-  };
+  const [inputValue, setInputValue] = useState({
+    id: '',
+    pw: '',
+  });
+
+  //const navigate = useNavigate();
+  console.log(inputValue);
+  //const onLogin = () => {
+  useEffect(() => {
+    fetch('http://10.58.0.163:8000/users/signin', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: inputValue.id,
+        password: inputValue.pw,
+      }),
+    })
+      .then(res => res.json())
+      .then(res => {
+        // if (res.token) {
+        //   //navigate('/Main/Main');
+        console.log(res);
+        // } else {
+        //   console.log(res);
+        // }
+      });
+  }, []);
+  //};
+
+  // function handlePwInput(event) {
+  //   const { value } = event.target;
+  //   console.log(value);
+  //   setPw(value);
+  // setPw(event.target.value);
+  //}
+
+  function handleInputValue(e) {
+    const { name, value } = e.target;
+    setInputValue(inputValue => ({ ...inputValue, [name]: value }));
+    //console.log(e);
+  }
+
   return (
     <div className="login">
       <h1 className="login-title">LOGIN</h1>
@@ -18,16 +55,40 @@ const Login = () => {
       </div>
 
       <div className="member-login">
-        <div className="member-login-title">회원 로그인</div>
+        <h2 className="member-login-title">회원 로그인</h2>
+
         <div className="login-container">
-          <form className="id-box">
-            <span className="id-title">아이디</span>
-            <input type="text" />
-            <span className="pw-title">비밀번호</span>
-            <input type="password" />
-            <button onClick={onLogin} className="login-button">
-              로그인
-            </button>
+          <form className="login-table">
+            <table className="table-box">
+              <tr className="id-box">
+                <th className="id-title">아이디</th>
+                <td>
+                  <input
+                    clssName="id-input"
+                    type="text"
+                    name="id"
+                    onChange={handleInputValue}
+                  />
+                </td>
+              </tr>
+
+              <tr className="pw-box">
+                <th className="pw-title">비밀번호</th>
+                <td>
+                  <input
+                    clssName="pw-input"
+                    name="pw"
+                    type="password"
+                    onChange={handleInputValue}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <button onSubmit={useEffect} className="login-button">
+                  로그인
+                </button>
+              </tr>
+            </table>
           </form>
         </div>
       </div>
