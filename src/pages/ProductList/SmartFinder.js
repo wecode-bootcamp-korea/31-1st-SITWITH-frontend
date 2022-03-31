d1a5acdcea19c4d5ce5570b30a285b9bd9d9665e
minsu <cocacolazero024@gmail.com>
2022-03-31 11:36:20 +0900

    MODIFY : smart finder css 수정 및 브랜치 이동 전 커밋

--- src/pages/ProductList/SmartFinder.js
+++ src/pages/ProductList/SmartFinder.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './SmartFinder.scss';
-import ProductListShow from './ProductListShow';
 
-const ProductList = () => {
+const SmartFinder = () => {
   const [chairs, setChairs] = useState([]);
   const [usetSearchInput, setUserSearchInput] = useState('');
   const [selected, setSelected] = useState('selected');
@@ -35,82 +34,71 @@ const ProductList = () => {
   ];
 
   return (
-    <div className="product-list-page">
-      <div className="product-list-container">
-        <h1 className="product-list-title">모든제품보기</h1>
-        <div className="separate-bar"></div>
-        <div className="smart-finder-container">
-          <h2>SMART FINDER</h2>
+    <div className="smart-finder-container">
+      <h2>SMART FINDER</h2>
 
-          <div className="smart-finder">
-            {/* 가격파트 */}
-            <div className="finder-price-container">
-              <span className="finder-title">가격(만원)</span>
+      <div className="smart-finder">
+        {/* 가격파트 */}
+        <div className="finder-price-container">
+          <span className="finder-title">가격(만원)</span>
 
-              <div className="finder-price">
-                {priceList.map(price => (
-                  <div key={price} className={`price price-${price}`}>
-                    <label>
-                      <input type="checkbox" name="color" value="blue" />
-                    </label>
-                    <div className="finder-select-bar"></div>
-                    <span>{price}</span>
-                  </div>
-                ))}
+          <div className="finder-price">
+            {priceList.map(price => (
+              <div key={price} className={`price price-${price}`}>
+                <label>
+                  <input type="checkbox" name="color" value="blue" />
+                </label>
+                <div className="finder-select-bar"></div>
+                <span>{price}</span>
               </div>
-            </div>
-
-            {/* 카테고리 */}
-            <div className="finder-category-container">
-              <span className="finder-title">카테고리</span>
-              <div className="finder-category">
-                {category.map(categories => (
-                  <div
-                    key={categories}
-                    className={`category category-${categories} ${selected}`}
-                    onClick={() => {
-                      selected == 'selected'
-                        ? setSelected('notSelected')
-                        : setSelected('selected');
-                    }}
-                  >
-                    <div className="finder-select-bar"></div>
-                    <span>{categories}</span>
-                  </div>
-                ))}
-              </div>
-            </div>
-
-            {/* 색상 */}
-            <div className="finder-color-container">
-              <span className="finder-title">색상</span>
-              <div className="finder-color">
-                {colorObject.map(colors => (
-                  <div
-                    key={Object.keys(colors)}
-                    className={`color color-${Object.values(colors)}`}
-                  >
-                    <div
-                      className={`finder-color-bar ${Object.values(colors)}`}
-                    ></div>
-                    <span>{Object.keys(colors)}</span>
-                  </div>
-                ))}
-              </div>
-            </div>
-
-            {/* 검색결과 */}
-            <div className="search-result">{}개의 제품이 검색되었습니다.</div>
+            ))}
           </div>
         </div>
-        <div className="product-list"></div>
+
+        {/* 카테고리 */}
+        <div className="finder-category-container">
+          <span className="finder-title">카테고리</span>
+          <div className="finder-category">
+            {category.map(categories => (
+              <div
+                key={categories}
+                className={`category category-${categories} ${selected}`}
+                onClick={() => {
+                  selected == 'selected'
+                    ? setSelected('notSelected')
+                    : setSelected('selected');
+                }}
+              >
+                <div className="finder-select-bar"></div>
+                <span>{categories}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* 색상 */}
+        <div className="finder-color-container">
+          <span className="finder-title">색상</span>
+          <div className="finder-color">
+            {colorObject.map(colors => (
+              <div
+                key={Object.keys(colors)}
+                className={`color color-${Object.values(colors)}`}
+              >
+                <div
+                  className={`finder-color-bar ${Object.values(colors)}`}
+                ></div>
+                <span>{Object.keys(colors)}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* 검색결과 */}
+        <div className="search-result">{}개의 제품이 검색되었습니다.</div>
       </div>
-      <ProductListShow
-      // userSearch={handleUserSearch}
-      // searchProduct={searchProduct}
-      />
     </div>
   );
 };
 
-export default ProductList;
+export default SmartFinder;
