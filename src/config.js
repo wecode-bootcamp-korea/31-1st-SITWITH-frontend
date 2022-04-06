79052f684f2e1db8e22dbf2d0cbc0cfde0b373a5
kyunghyunKim <hyunshiness@gmail.com>
2022-04-06 17:36:22 +0900

    ADD : conpig 파일 생성

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const BASE_URL = 'http://10.58.1.90:8000';
+export const API = {
+  login: `${BASE_URL}/login`,
+  join: `${BASE_URL}/join`,
+  product: `${BASE_URL}/product`,
+  products: `${BASE_URL}/products`,
+  cart: `${BASE_URL}/cart`,
+};
