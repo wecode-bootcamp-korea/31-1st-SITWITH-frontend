836ba9cede65bf0c228bb90e91d80843256f3536
bonimac <bonimac@bonimacui-MacBookPro.local>
2022-04-06 16:52:30 +0900

    MODIFY : ui map 컴포넌트분리

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ProductMenu from './ProductMenu';
+import ServiceMenu from './ServiceMenu';
 import './Nav.scss';
 
 const Nav = () => {
   return (
     <nav className="nav">
       <div className="nav-top">
-        <ul className="sub-menu-list">
-          {MENU_DATA.service.map(menu => (
-            <li className="sub-menu-item" key={menu.title}>
-              <Link className="link-menu" to={menu.link}>
-                <i className={`ico-nav ico-nav-${menu.icon}`} />
-                <span className="txt">{menu.title}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <ServiceMenu />
       </div>
       <div className="nav-bottom">
         <div className="left-area">
@@ -26,36 +19,12 @@ const Nav = () => {
         <div className="right-area">
           <div className="product-menu">
             <div className="menu-title">PRODUCTS</div>
-            <ul className="product-menu-list">
-              {MENU_DATA.product.map(menu => (
-                <li className="product-menu-item" key={menu.title}>
-                  <Link className="link-product" to={menu.link}>
-                    {menu.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <ProductMenu />
           </div>
         </div>
       </div>
     </nav>
   );
-};
-
-const MENU_DATA = {
-  service: [
-    { title: '로그인', link: '/Login' },
-    { title: '회원가입', link: '/Join' },
-    { title: '비교하기', link: '/Compare', icon: 'compare' },
-    { title: '장바구니', link: '/Cart', icon: 'cart' },
-  ],
-  product: [
-    { title: '모든 제품보기', link: '/ProductList' },
-    { title: '사무용 의자', link: '/ProductList/category/office' },
-    { title: '학생용 의자', link: '/ProductList/category/study' },
-    { title: '유아용 의자', link: '/ProductList/category/baby' },
-    { title: '중역용 의자', link: '/ProductList/category/executive' },
-  ],
 };
 
 export default Nav;
