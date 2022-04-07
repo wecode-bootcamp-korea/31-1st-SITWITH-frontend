87aa839a8c2eed548fa9281477f37911c4d537fb
kyunghyunKim <hyunshiness@gmail.com>
2022-04-07 11:43:05 +0900

    MODIFY : API 경로수정 및 scss 수정

--- src/pages/Join/Join.js
+++ src/pages/Join/Join.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Join.scss';
-import { BASE_URL } from '../../config';
+import { API } from '../../config';
 
 const Join = () => {
   const [joinValue, setJoinValue] = useState({
@@ -33,7 +33,7 @@ const Join = () => {
     } else if (!validEmail) {
       alert('[ 이메일 ] 은(는) 공백없이 @/. 을 조합하여 입력해야 합니다');
     } else {
-      fetch(`${BASE_URL}/users/signup`, {
+      fetch(`${API.join}`, {
         method: 'POST',
         body: JSON.stringify({
           username: id,
