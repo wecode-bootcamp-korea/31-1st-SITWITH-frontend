d07b31d3527d2a6ca6585a7f4192cadb89c1202c
kyunghyunKim <hyunshiness@gmail.com>
2022-04-07 11:48:19 +0900

    MODIFY : config 컴포넌트 경로 수정

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const BASE_URL = 'http://10.58.1.90:8000';
 export const API = {
-  login: `${BASE_URL}/login`,
-  join: `${BASE_URL}/join`,
+  login: `${BASE_URL}/users/signin`,
+  join: `${BASE_URL}/users/signup`,
   product: `${BASE_URL}/product`,
   products: `${BASE_URL}/products`,
   carts: `${BASE_URL}/carts`,
